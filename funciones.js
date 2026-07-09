e2d4cb851b9f47797f1fe24bdeb08ab6b7aec41c
Ramon Code <rcalero5@xtec.cat>
2026-07-09 21:05:46 +0200

    Afegit selector d'aules i càrrega d'alumnes

--- funciones.js
+++ funciones.js
@@ -1,7 +1,19 @@
+const full = SpreadsheetApp.openById('1FzOhlhpPKt8nESIGZRkVAJF4ErkjYuYOcleWpskB_60');
+
 function doGet() {
     return HtmlService.createTemplateFromFile('index').evaluate().setTitle('Absències').setFaviconUrl('https://cdn-icons-png.flaticon.com/512/3135/3135715.png');
 }
 
-function obtenerDatosHTML(nombre) {
-    return HtmlService.createHtmlOutputFromFile(nombre).getContent();
+function obtenirDadesHTML(nom) {
+    return HtmlService.createHtmlOutputFromFile(nom).getContent();
+}
+
+function obtenirAlumnes(aula) {
+    const fulla = full.getSheetByName(aula);
+    const alumnes = fulla.getDataRange().getValues();
+    return alumnes;
+}
+
+function obtenirAules() {
+  return full.getSheets().map(f => f.getName());
 }
