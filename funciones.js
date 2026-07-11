203b7e7809f893d5d007744266bb18dfff38971a
Ramon Code <rcalero5@xtec.cat>
2026-07-11 18:45:43 +0200

    millorar el codi de les notificacions i canviar la id de global a local

--- funciones.js
+++ funciones.js
@@ -1,5 +1,3 @@
-const full = SpreadsheetApp.openById('1FzOhlhpPKt8nESIGZRkVAJF4ErkjYuYOcleWpskB_60');
-
 function doGet() {
     return HtmlService.createTemplateFromFile('index').evaluate().setTitle('Absències').setFaviconUrl('https://cdn-icons-png.flaticon.com/512/3135/3135715.png');
 }
@@ -9,11 +7,11 @@ function obtenirDadesHTML(nom) {
 }
 
 function obtenirAlumnes(aula) {
-    const fulla = full.getSheetByName(aula);
+    const fulla = SpreadsheetApp.openById('1FzOhlhpPKt8nESIGZRkVAJF4ErkjYuYOcleWpskB_60').getSheetByName(aula);
     const alumnes = fulla.getDataRange().getValues();
     return alumnes;
 }
 
 function obtenirAules() {
-    return full.getSheets().map(f => f.getName());
+    return SpreadsheetApp.openById('1FzOhlhpPKt8nESIGZRkVAJF4ErkjYuYOcleWpskB_60').getSheets().map(f => f.getName());
 }
